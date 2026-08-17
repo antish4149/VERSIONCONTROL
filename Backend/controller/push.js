b0c16e9906b2577bf6f3b1f01294c01a32542182
Antish Kumar Yadav <antishyadav726@gmail.com>
2026-08-18 00:42:20 +0530

    push implemented

--- Backend/controller/push.js
+++ Backend/controller/push.js
@@ -1,4 +1,33 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { s3, S3_BUCKET } from '../config/aws.config.js';
+
 const pushRepo = async () => {
-    console.log("Repo pushed");
+    const repoPath = path.resolve(process.cwd(), ".gitFolder");
+    const commitsPath = path.join(repoPath, "commits");
+    try {
+        const commitDirs = await fs.readdir(commitsPath);
+
+        for (const commitDir of commitDirs) {
+
+            const commitPath = path.join(commitsPath, commitDir);
+            const files = await fs.readdir(commitPath);
+
+            for (const file of files) {
+                const filePath = path.join(commitPath, file);
+                const fileContent = await fs.readFile(filePath);
+                const params = {
+                    Bucket: S3_BUCKET,
+                    Key: `commits/${commitDir}/${file}`,
+                    Body: fileContent,
+                };
+                await s3.upload(params).promise();
+            }
+        }
+
+        console.log("All commits pushed successfully to S3!");
+    } catch (error) {
+        console.log(`Push error:${error}`);
+    }
 }
 export default pushRepo;
