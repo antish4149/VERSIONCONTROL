d2b3c08bb18abcc87efda41ff341d7e3ff7155a9
Antish Kumar Yadav <antishyadav726@gmail.com>
2026-08-27 23:52:08 +0530

    working with repo functionality

--- Backend/controller/repoController.js
+++ Backend/controller/repoController.js
@@ -1,33 +1,60 @@
+import mongoose from "mongoose";
+import Repository from "../models/repoModel.js";
+import User from "../models/userModel.js";
+import Issue from "../models/issueModel.js";
 
-const createRepository = (req, res) => {
-    res.send("Respository created");
+const createRepository = async (req, res) => {
+
+    const { name, description, content, visibility, owner, } = req.body;
+
+    try {
+        if (!name) {
+            return res.status(400).json({ message: "Repository name is required" });
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(owner)) {
+            return res.status(400).json({ message: "Invalid owner ID" });
+        }
+
+        const user = await User.findById(owner);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const repository = await Repository.create({ name, description, content, visibility, owner });
+        res.status(201).json(repository);
+    } catch (error) {
+        console.log("Error during repository creation", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
-const getRepository = (req, res) => {
+const getRepository = async (req, res) => {
     res.send("Respository fetched");
 }
 
-const getRepositoryById = (req, res) => {
+const getRepositoryById = async (req, res) => {
     res.send("Respository fetched by ID");
 }
 
-const getRepositoryByName = (req, res) => {
+const getRepositoryByName = async (req, res) => {
     res.send("Respository fetched by Name");
 }
 
-const updateRepository = (req, res) => {
+const updateRepository = async (req, res) => {
     res.send("Respository updated");
 }
 
-const getRepositoryForCurrUser = (req, res) => {
+const getRepositoryForCurrUser = async (req, res) => {
     res.send("Respository fetched for current user");
 }
 
-const deleteRepository = (req, res) => {
+const deleteRepository = async (req, res) => {
     res.send("Respository deleted");
 }
 
-const toogleVisibility = (req, res) => {
+const toogleVisibility = async (req, res) => {
     res.send("Visibility toggled");
 }
 
