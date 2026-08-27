ba5d2ee5b9ef61ed45ccd4567f00b4c7a51682a4
Antish Kumar Yadav <antishyadav726@gmail.com>
2026-08-28 01:27:05 +0530

    fix(routes): rename reop/name to repo/name/:name

--- Backend/routes/repoRouter.js
+++ Backend/routes/repoRouter.js
@@ -5,11 +5,11 @@ const repoRouter = express.Router();
 
 repoRouter.post("/repo/create", repoController.createRepository);
 repoRouter.get("/repo/all", repoController.getRepository);
-repoRouter.get("/repp/:id", repoController.getRepositoryById);
-repoRouter.get("/repo/:name", repoController.getRepositoryByName);
-repoRouter.get("/repo/user", repoController.getRepositoryForCurrUser);
-repoRouter.put("/repo/:id", repoController.updateRepository);
-repoRouter.delete("/repo/:id", repoController.deleteRepository);
+repoRouter.get("/repo/:id", repoController.getRepositoryById);
+repoRouter.get("/repo/name/:name", repoController.getRepositoryByName);
+repoRouter.get("/repo/user/:userId", repoController.getRepositoryForCurrUser);
+repoRouter.put("/repo/update/:id", repoController.updateRepository);
+repoRouter.delete("/repo/delete/:id", repoController.deleteRepository);
 repoRouter.patch("/repo/toggle/:id", repoController.toogleVisibility);
 
 export default repoRouter;
